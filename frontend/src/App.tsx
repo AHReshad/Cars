09d792ef1b3c948c9231e740f611ea18fc708a2e
ReshadAhmadi010 <reshadahmadi2001@gmail.com>
2025-08-04 13:51:16 +0430

    changes login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ import Services from "./page/services/Services.tsx";
 import Create from "./page/create-yours/Create.tsx";
 import { AccordionDemo } from "./page/Faq/Faq.tsx";
 import Landingpage from "./page/Landingpage/Landingpage.tsx";
+import Login from "./page/Form/Login.tsx";
+import Signup from "./page/Form/Signup.tsx";
 
 export default function App() {
   return (
@@ -25,6 +27,8 @@ export default function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/create" element={<Create />} />
         <Route path="/Faq" element={<AccordionDemo />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
       {/* <Footer /> */}
     </Router>
